67a4e956e46eb4d795f2c5edf0b1dfe437cda8ec
Victor Shyba <victor.shyba@gmail.com>
2016-05-06 11:13:27 -0300

    Extract create call logic to private method

--- app/Http/Controllers/ConferenceController.php
+++ app/Http/Controllers/ConferenceController.php
@@ -33,15 +33,7 @@ class ConferenceController extends Controller
 
         $twilioNumber = config('services.twilio')['number'];
         $path = str_replace($request->path(), '', $request->url()) . 'conference/connect/' . $conference_id . '/agent1';
-        try {
-            $client->account->calls->create(
-                $twilioNumber, // The number of the phone initiating the call
-                'client:agent1', // The agent_id that will receive the call
-                $path // The URL Twilio will request when the call is answered
-            );
-        } catch (Exception $e) {
-            return 'Error: ' . $e->getMessage();
-        }
+        $this->call('agent1', $path, $client);
 
         $active_call = ActiveCall::firstOrNew(['agent_id' => 'agent1']);
         $active_call->conference_id = $conference_id;
@@ -76,11 +68,17 @@ class ConferenceController extends Controller
         $twilioNumber = config('services.twilio')['number'];
         $conference_id = ActiveCall::where('agent_id', $agent_id)->first()->conference_id;
         $path = str_replace($request->path(), '', $request->url()) . 'conference/connect/' . $conference_id . '/agent2';
+        return $this->call('agent2', $path, $client);
+    }
+
+    private function call($agent_id, $callback_path, $client) {
+        $destinationNumber = 'client:' . $agent_id;
+        $twilioNumber = config('services.twilio')['number'];
         try {
             $client->account->calls->create(
                 $twilioNumber, // The number of the phone initiating the call
-                'client:agent2', // The agent_id that will receive the call
-                $path // The URL Twilio will request when the call is answered
+                'client:' . $agent_id, // The agent_id that will receive the call
+                $callback_path // The URL Twilio will request when the call is answered
             );
         } catch (Exception $e) {
             return 'Error: ' . $e->getMessage();
